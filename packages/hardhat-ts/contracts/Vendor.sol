270b1353b0a80b9bdebf8130755c9b3dcb80f798
Bori7 <oluwaboriife@gmail.com>
2022-04-02 16:29:43 +0100

    challenge-2 going in

--- packages/hardhat-ts/contracts/Vendor.sol
+++ packages/hardhat-ts/contracts/Vendor.sol
@@ -1,19 +1,45 @@
 pragma solidity >=0.8.0 <0.9.0;
 // SPDX-License-Identifier: MIT
 
-//import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import './YourToken.sol';
 
-contract Vendor {
+contract Vendor is Ownable{
   YourToken public yourToken;
+  
+  uint256 public constant tokensPerEth = 100;
 
-  constructor(address tokenAddress) public {
+  constructor(address tokenAddress) {
     yourToken = YourToken(tokenAddress);
   }
+  event SellTokens(address indexed seller, uint256 amountOfTokens, uint256 amountOfETH);
+  event BuyTokens(address indexed buyer, uint256 amountOfETH, uint256 amountOfTokens);
 
   // ToDo: create a payable buyTokens() function:
+  function buyTokens() public payable{
+
+    require(msg.value > 0, "You need to send some ether"); // check if the sender sent ether
+    uint256 amountTobuy = msg.value * tokensPerEth; // how much tokens to buy
+
+    yourToken.transfer(msg.sender, amountTobuy); // transfer the tokens to the receiver
+    emit BuyTokens(msg.sender, msg.value, amountTobuy); // emit the event
+  }
 
   // ToDo: create a withdraw() function that lets the owner withdraw ETH
+  function withdraw() public onlyOwner{
+    require(address(this).balance > 0, "You don't have enough ether to withdraw"); // check if the owner has enough ether
+    (bool taken,) = msg.sender.call{value: address(this).balance}(""); 
+    require(taken, "withdrawal unsuccessful"); // check if the transfer was successful
+  }
 
   // ToDo: create a sellTokens() function:
+    function sellTokens(uint256 amountToken) public {
+      uint256 balances = yourToken.balanceOf(msg.sender);
+      require(balances >= amountToken, "You don't have enough tokens to sell"); // check if the sender has enough tokens
+      uint256 saleEth = amountToken/tokensPerEth;
+      yourToken.transferFrom(msg.sender, address(this), amountToken);
+      (bool sold, ) = msg.sender.call{value: saleEth}("");
+      require(sold, "sale unsuccessful");
+      emit SellTokens(msg.sender, amountToken, saleEth);
+  }
 }
