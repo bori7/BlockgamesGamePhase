c008cc9719a228fa4cba1de1714bf376533eea5f
Bori7 <oluwaboriife@gmail.com>
2022-04-02 11:54:19 +0100

    challenge-1 going in

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -16,7 +16,12 @@ contract Staker {
 
   uint256 public constant threshold = 1 ether;
 
-  event Stake(address addr,uint256 amnt); // event for staking
+  bool public openForWithdraw = false;
+
+  // uint256 public deadline = block.timestamp + 10 minutes;
+  uint256 public deadline = block.timestamp + 72 hours;
+
+  event Stake(address indexed addr, uint256 amnt); // event for staking
 
   // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
   //  ( make sure to add a `Stake(address,uint256)` event and emit it for the frontend <List/> display )
@@ -30,6 +35,41 @@ contract Staker {
         emit Stake(msg.sender, msg.value);
     }
 
+    function execute() public notCompleted{
+      if(address(this).balance >= threshold){
+        // exampleExternalContract.complete();
+        exampleExternalContract.complete{value: address(this).balance}();
+      }else{
+        openForWithdraw = true;
+      }
+    }
+
+    function withdraw() public notCompleted{
+      uint256 userBalance = balances[msg.sender]; 
+      require(userBalance > 0, "You haven't staked anything yet");
+ 
+      balances[msg.sender] = 0; 
+      (bool withdrawn,) = msg.sender.call{value: userBalance}("");
+      require(withdrawn, "Withdrawal failed");
+    }
+
+    function timeLeft() public view returns (uint256) {
+      if (block.timestamp >= deadline){
+        return 0;
+      }else{
+        return deadline - block.timestamp;
+      }
+    }
+
+    receive() external payable{
+        return stake();
+    }
+
+    modifier notCompleted() {
+      bool completed = exampleExternalContract.completed();
+      require(!completed, "staking process already accomplished");
+      _;
+    }
 
   // After some `deadline` allow anyone to call an `execute()` function
   //  It should either call `exampleExternalContract.complete{value: address(this).balance}()` to send all the value
